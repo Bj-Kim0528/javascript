1c2db611fb96808f0f15b596b9f625d6a178d462
EC2 Default User <bjking102409@gmail.com>
2025-07-18 04:46:33 +0000

    upDownGame002

--- app.js
+++ app.js
@@ -13,8 +13,15 @@ let computerNum = 0
 let playButton = document.getElementById("play-button")
 let userInput = document.getElementById("user-input")
 let resultArea = document.getElementById("result-area")
+let resetButton = document.getElementById("reset-button")
+let chances = 5
+let gameOver = false
+let chanceArea = document.getElementById("chance-area")
+let history=[]
 
 playButton.addEventListener("click",play)//함수를 매개변수로 넘김
+resetButton.addEventListener("click",reset)
+userInput.addEventListener("focus",function(){userInput.value=""})
 
 function pickRandonNum(){
   computerNum = Math.floor(Math.random()*100)+1;//0~1사이의 랜덤숫자를 뽑아줌
@@ -23,13 +30,50 @@ function pickRandonNum(){
 
 function play(){
   let userValue = userInput.value
+
+  if(userValue<1 || userValue>100){
+    resultArea.textContent="1과 100사이의 값입력"
+    return
+  }
+  if(history.includes(userValue)){
+    resultArea.textContent="이미입력함"
+    return
+  }
+
+  chances -- ;
+  chanceArea.textContent=`남은기회:${chances}번`
+  console.log("찬스는", chances)
+
   if(userValue < computerNum){
     resultArea.textContent = "UP!!"
   }else if(userValue > computerNum){
     resultArea.textContent = "DOWN!!"
   }else{
     resultArea.textContent = "OK!!"
+    gameOver = true
   }
+
+  history.push(userValue)
+  console.log(history)
+
+  if(chances < 1){
+    gameOver = true
+  }
+  if(gameOver ==true){
+    playButton.disabled = true
+  }
+}
+
+function reset(){
+  //user input창이 깨긋이 정리됨
+  userInput.value = ""
+  //새로운번호가 생성됨
+  pickRandonNum()
+  gameOver = false
+  playButton.disabled = false
+  chances = 5
+  chanceArea.textContent=`남은기회:${chances}번`
+  resultArea.textContent="결과값이 여기로 나옵니다"
 }
 pickRandonNum();
 
